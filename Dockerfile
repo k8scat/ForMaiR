56311646bcd8e8a1bfccb72add54b8a7d3c4eb99
K8sCat <k8scat@gmail.com>
2021-02-14 08:00:49 +0800

    :hammer: update release dockerhub

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM python:3.6-alpine
 
 LABEL maintainer="K8sCat <k8scat@gmail.com>"
-LABEL version="1.3.0"
+LABEL version="1.3.1"
 
 WORKDIR /formair
 
 COPY . .
 
-RUN python -m pip install --upgrade pip setuptools && \
-pip install -r requirements.txt && \
+RUN python -m pip install --upgrade pip && \
+python -m pip install --upgrade setuptools && \
+python -m pip install -r requirements.txt && \
 python setup.py install
 
 ENTRYPOINT [ "formair" ]
--- scripts/pipeline/docker_release.groovy
+++ scripts/pipeline/docker_release.groovy
@@ -2,6 +2,15 @@ pipeline {
     agent {
         label 'master'
     }
+
+    parameters {
+        string (
+            name: 'tag',
+            defaultValue: 'latest',
+            description: 'build tag'
+        )
+    }
+
     stages {
         stage('Release to DockerHub') {
             steps {
@@ -10,11 +19,11 @@ pipeline {
                     passwordVariable: 'password',
                     usernameVariable: 'username')
                 ]) {
-                    sh '''
-                    docker login -u $username -p $password
-                    docker build -t formair:latest .
+                    sh """
+                    docker login -u ${username} -p ${password}
+                    docker build -t k8scat/formair:${params.tag} .
                     docker push k8scat/formair:latest
-                    '''
+                    """
                 }
             }
         }
